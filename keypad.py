09f9a5e589c2eb0f7713c490b309d2fe101cc541
Jordan Cook <jordan.cook.git@proton.me>
2022-12-30 17:21:05 -0600

    WIP: A bit of cleanup

--- keypad.py
+++ keypad.py
@@ -1,13 +1,12 @@
-# SPDX-FileCopyrightText: 2021 John Park for Adafruit Industries
-# SPDX-License-Identifier: MIT
-# NUMPAD 4000! Made with snap-apart NeoKey PCB and Feather RP2040.
-
+# Modified from:
+#   https://learn.adafruit.com/numpad-4000-mechanical-keyswitch-data-entry-device
 import board
-import keypad
-import neopixel
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
+from neopixel import NeoPixel
+
+from keypad import KeyMatrix
 
 COLUMNS = 5
 ROWS = 5
@@ -17,13 +16,13 @@ WHITE = 0x303030
 RED = 0xFF0000
 
 
-board_pix = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.1)
+board_pix = NeoPixel(board.NEOPIXEL, 1, brightness=0.1)
 board_pix[0] = BLUE
 
-key_pixels = neopixel.NeoPixel(board.D5, 30, brightness=0.1)
+key_pixels = NeoPixel(board.D5, 30, brightness=0.1)
 key_pixels.fill(WHITE)
 
-keys = keypad.KeyMatrix(
+keys = KeyMatrix(
     row_pins=(board.D4, board.A3, board.A2, board.A1, board.A0),
     column_pins=(board.D13, board.D12, board.D11, board.D10, board.D9),
     columns_to_anodes=False,
@@ -31,49 +30,47 @@ keys = keypad.KeyMatrix(
 
 kbd = Keyboard(usb_hid.devices)
 
+# fmt: off
 keycode_LUT = [
-             0, 1, 2, 3, 4,
-             5, 6, 7, 8,
-             10, 11, 12, 13, 14,
-             15, 16, 17, 18,
-             20, 21, 23, 24
+    0, 1, 2, 3, 4,
+    5, 6, 7, 8,
+    10, 11, 12, 13, 14,
+    15, 16, 17, 18,
+    20, 21, 23, 24
 ]
-
 pixel_LUT = [
-             0, 1, 2, 3, 4,
-             8, 7, 6, 5,
-             10, 11, 12, 13, 14,
-             18, 17, 16, 15,
-             20, 21, 23, 24
+    0, 1, 2, 3, 4,
+    8, 7, 6, 5,
+    10, 11, 12, 13, 14,
+    18, 17, 16, 15,
+    20, 21, 23, 24
 ]
-# create a keycode dictionary including modifier state and keycodes
+# fmt: on
+
+# keycode dictionary including modifier state and keycodes
 keymap = {
-            (0): (0, Keycode.KEYPAD_NUMLOCK),
-            (1): (0, Keycode.BACKSPACE),
-            (2): (0, Keycode.FORWARD_SLASH),
-            (3): (0, Keycode.KEYPAD_ASTERISK),
-            (4): (0, Keycode.KEYPAD_MINUS),
-
-            (5): (0, Keycode.PAGE_UP),
-            (6): (0, Keycode.KEYPAD_SEVEN),
-            (7): (0, Keycode.KEYPAD_EIGHT),
-            (8): (0, Keycode.KEYPAD_NINE),
-
-            (9): (0, Keycode.PAGE_DOWN),
-            (10): (0, Keycode.KEYPAD_FOUR),
-            (11): (0, Keycode.KEYPAD_FIVE),
-            (12): (0, Keycode.KEYPAD_SIX),
-            (13): (0, Keycode.KEYPAD_PLUS),
-
-            (14): (1, Keycode.SHIFT),
-            (15): (0, Keycode.KEYPAD_ONE),
-            (16): (0, Keycode.KEYPAD_TWO),
-            (17): (0, Keycode.KEYPAD_THREE),
-
-            (18): (2, Keycode.CONTROL),
-            (19): (0, Keycode.KEYPAD_ZERO),
-            (20): (0, Keycode.KEYPAD_PERIOD),
-            (21): (0, Keycode.KEYPAD_EQUALS)  # KEYPAD_ENTER on non-mac
+    0: Keycode.KEYPAD_NUMLOCK,
+    1: Keycode.BACKSPACE,
+    2: Keycode.FORWARD_SLASH,
+    3: Keycode.KEYPAD_ASTERISK,
+    4: Keycode.KEYPAD_MINUS,
+    5: Keycode.PAGE_UP,
+    6: Keycode.KEYPAD_SEVEN,
+    7: Keycode.KEYPAD_EIGHT,
+    8: Keycode.KEYPAD_NINE,
+    9: Keycode.PAGE_DOWN,
+    10: Keycode.KEYPAD_FOUR,
+    11: Keycode.KEYPAD_FIVE,
+    12: Keycode.KEYPAD_SIX,
+    13: Keycode.KEYPAD_PLUS,
+    14: Keycode.SHIFT,
+    15: Keycode.KEYPAD_ONE,
+    16: Keycode.KEYPAD_TWO,
+    17: Keycode.KEYPAD_THREE,
+    18: Keycode.CONTROL,
+    19: Keycode.KEYPAD_ZERO,
+    20: Keycode.KEYPAD_PERIOD,
+    21: Keycode.KEYPAD_EQUALS,
 }
 
 shift_mod = False
@@ -81,43 +78,36 @@ ctrl_mod = False
 
 
 while True:
-
     key_event = keys.events.get()
-    if key_event:
-        if key_event.pressed:
-            if keymap[keycode_LUT.index(key_event.key_number)][0] == 1:
-                shift_mod = True
-            elif keymap[keycode_LUT.index(key_event.key_number)][0] == 2:
-                ctrl_mod = True
-            if shift_mod is False and ctrl_mod is False:
-                kbd.press(keymap[keycode_LUT.index(key_event.key_number)][1])
-                print(keymap[keycode_LUT.index(key_event.key_number)][1])
-                key_pixels[pixel_LUT.index(key_event.key_number)] = RED
-            elif shift_mod is True and ctrl_mod is False:
-                kbd.press(Keycode.SHIFT, keymap[keycode_LUT.index(key_event.key_number)][1])
-                print(keymap[keycode_LUT.index(key_event.key_number)][1])
-                key_pixels[pixel_LUT.index(key_event.key_number)] = RED
-            elif shift_mod is False and ctrl_mod is True:
-                kbd.press(Keycode.CONTROL, keymap[keycode_LUT.index(key_event.key_number)][1])
-                print(keymap[keycode_LUT.index(key_event.key_number)][1])
-                key_pixels[pixel_LUT.index(key_event.key_number)] = RED
-            elif shift_mod is True and ctrl_mod is True:
-                kbd.press(
-                          Keycode.SHIFT,
-                          Keycode.CONTROL,
-                          keymap[keycode_LUT.index(key_event.key_number)][1]
-                          )
-                print(keymap[keycode_LUT.index(key_event.key_number)][1])
-                key_pixels[pixel_LUT.index(key_event.key_number)] = RED
-            board_pix[0] = WHITE
+    if not key_event:
+        continue
 
-        if key_event.released:
-            if keymap[keycode_LUT.index(key_event.key_number)][0] == 1:  # un-shift
-                shift_mod = False
-            elif keymap[keycode_LUT.index(key_event.key_number)][0] == 2:  # un-ctrl
-                ctrl_mod = False
+    keycode = keymap[keycode_LUT.index(key_event.key_number)]
+    pixel_idx = pixel_LUT.index(key_event.key_number)
+    keypresses = [keycode]
 
-            kbd.release(keymap[keycode_LUT.index(key_event.key_number)][1])
-            key_pixels[pixel_LUT.index(key_event.key_number)] = WHITE
-            board_pix[0] = BLUE
+    if key_event.pressed:
+        if keycode == Keycode.SHIFT:
+            shift_mod = True
+        elif keycode == Keycode.CONTROL:
+            ctrl_mod = True
 
+        if ctrl_mod:
+            keypresses.insert(0, Keycode.CONTROL)
+        if shift_mod:
+            keypresses.insert(0, Keycode.SHIFT)
+
+        kbd.press(*keypresses)
+        print(keycode)
+        key_pixels[pixel_idx] = RED
+        board_pix[0] = WHITE
+
+    elif key_event.released:
+        if keycode == Keycode.SHIFT:
+            shift_mod = False
+        elif keycode == Keycode.CONTROL:
+            ctrl_mod = False
+
+        kbd.release(keycode)
+        key_pixels[pixel_idx] = WHITE
+        board_pix[0] = BLUE
