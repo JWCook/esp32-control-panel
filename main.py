96c29c11a1405de4345a84b2f8782f8c6b44d559
Jordan Cook <jordan.cook.git@proton.me>
2022-12-29 19:07:31 -0600

    Test toggle switch

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 import board
-from adafruit_seesaw import digitalio, neopixel, rotaryio, seesaw
+from adafruit_seesaw.digitalio import DigitalIO
+from adafruit_seesaw.neopixel import NeoPixel
+from adafruit_seesaw.rotaryio import IncrementalEncoder
+from adafruit_seesaw.seesaw import Seesaw
+from digitalio import DigitalInOut, Direction, Pull
 
 i2c = board.STEMMA_I2C()
 
-# qt_enc1 = seesaw.Seesaw(i2c, addr=0x36)
+# qt_enc1 = Seesaw(i2c, addr=0x36)
 # qt_enc1.pin_mode(24, qt_enc1.INPUT_PULLUP)
-# button1 = digitalio.DigitalIO(qt_enc1, 24)
-# encoder1 = rotaryio.IncrementalEncoder(qt_enc1)
+# button1 = DigitalIO(qt_enc1, 24)
+# encoder1 = IncrementalEncoder(qt_enc1)
 # last_position1 = None
 # button_held1 = False
-# pixel1 = neopixel.NeoPixel(qt_enc1, 6, 1)
+# pixel1 = NeoPixel(qt_enc1, 6, 1)
 # pixel1.brightness = 0.2
 # pixel1.fill(0xFF0000)
 
-# qt_enc2 = seesaw.Seesaw(i2c, addr=0x37)
+# qt_enc2 = Seesaw(i2c, addr=0x37)
 # qt_enc2.pin_mode(24, qt_enc2.INPUT_PULLUP)
-# button2 = digitalio.DigitalIO(qt_enc2, 24)
-# encoder2 = rotaryio.IncrementalEncoder(qt_enc2)
+# button2 = DigitalIO(qt_enc2, 24)
+# encoder2 = IncrementalEncoder(qt_enc2)
 # last_position2 = None
 # button_held2 = False
-# pixel2 = neopixel.NeoPixel(qt_enc2, 6, 1)
+# pixel2 = NeoPixel(qt_enc2, 6, 1)
 # pixel2.brightness = 0.2
 # pixel2.fill(0x0000FF)
 
-qt_enc3 = seesaw.Seesaw(i2c, addr=0x38)
+qt_enc3 = Seesaw(i2c, addr=0x38)
 qt_enc3.pin_mode(24, qt_enc3.INPUT_PULLUP)
-button3 = digitalio.DigitalIO(qt_enc3, 24)
-encoder3 = rotaryio.IncrementalEncoder(qt_enc3)
+button3 = DigitalIO(qt_enc3, 24)
+encoder3 = IncrementalEncoder(qt_enc3)
 last_position3 = None
 button_held3 = False
-pixel3 = neopixel.NeoPixel(qt_enc3, 6, 1)
+pixel3 = NeoPixel(qt_enc3, 6, 1)
 pixel3.brightness = 0.2
 pixel3.fill(0x00FF00)
+
+
+switch_1 = DigitalInOut(board.A0)
+switch_1.direction = Direction.INPUT
+switch_1.pull = Pull.UP
+last_switch_1 = None
 
 
 while True:
@@ -82,3 +92,7 @@ while True:
         button_held3 = False
         pixel3.brightness = 0.2
         print("Button 3 released")
+
+    if switch_1.value != last_switch_1:
+        last_switch_1 = switch_1.value
+        print("Switch 1: {}".format(switch_1.value))
